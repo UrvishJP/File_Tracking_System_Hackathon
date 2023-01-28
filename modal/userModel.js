4df22b1694395e01dd4e79fccf0e8f581c4c9fcd
Milan Padhariya <milan.padhariya4@gmail.com>
2023-01-28 13:00:55 +0530

    modified user model changes made:set ondesk default to false and better error response on role

--- modal/userModel.js
+++ modal/userModel.js
@@ -7,8 +7,8 @@ const userSchema=new mongoose.Schema(
         {
             type:String,
             enum:{
-                values:['user','admin'],
-                message:'enter valid role'
+                values:['User','Admin'],
+                message:'the role should be either User or Admin only'
             }
         },
         name:
@@ -27,7 +27,8 @@ const userSchema=new mongoose.Schema(
         },
         onDesk:
         {
-            type:Boolean
+            type:Boolean,
+            default:false
         }
     }
 )
